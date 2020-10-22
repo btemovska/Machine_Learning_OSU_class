aa4c3fd8e74fc42731c7785eebffdd5e2960135e
btemovska <btemovska@gmail.com>
2020-10-21 20:05:05 -0400

    Kaggle dataset picture pixels

---
+++ Fashion_Mninst_2.py
@@ -0,0 +1,23 @@
+import csv
+
+with open("C:/Users/btemo/Downloads/fashion-mnist_train.csv.zip/fashion-mnist_train.csv", newline='') as f:
+    reader = csv.reader(f)
+    row_count = 0
+    for row in reader:
+        if row_count==0:
+            row_count = row_count + 1
+            continue
+        example_pixel_list = row
+        print(row)
+        break
+    length_of_example_pixel_list = len(example_pixel_list)
+    print(length_of_example_pixel_list) #785
+f.close()
+
+
+#the data on the link is represented as a csv file in which each row is an image
+#the images are black and white 28x28 pixels images of clothes
+#each image row is 785 cells long
+#the first cell in each row represents label (which type of clothes it is)
+#the remaining 784 cells (28x28) represent pixels
+#each with value between 0 and 255
