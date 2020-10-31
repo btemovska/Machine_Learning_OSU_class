82b4cc020112ebc623892f23babf7ce8c4c2260b
btemovska <btemovska@gmail.com>
2020-10-30 21:06:32 -0400

    SVM for multiple classes classification

--- Non_linear_separable_svm.R
+++ Non_linear_separable_svm.R
@@ -37,3 +37,35 @@ ypred <- predict(svmfit, dat)
 
 
 
+
+#SVM for multiple classes classification
+# Adding 50 points as class 3
+x <- rbind(x, matrix(rnorm(50*2), ncol = 2))
+length(x) ### [1] 500
+
+y <- c(y, rep(0,50))
+x[y==0,2] <- x[y==0,2] + 2.5
+dat <- data.frame(x=x, y=as.factor(y))
+# plot data set
+ggplot(data = dat, aes(x = x.2, y = x.1, color = y, shape = y)) +
+  geom_point(size = 2) +
+  scale_color_manual(values=c("#000000","#FF0000","#00BA00")) +
+  theme(legend.position = "none")
+
+# fit model
+svmfit <- svm(y~., data = dat, kernel = "radial", cost = 10, gamma = 1)
+# plot results
+plot(svmfit, dat)
+
+svmfit
+ypred <- predict(svmfit, dat)
+(misclass <- table(predict = ypred, truth = dat$y))
+
+#         truth
+#predict  0   1   2
+#         0  33   4   3
+#         1  13 141  10
+#         2   4   5  37
+
+
+
